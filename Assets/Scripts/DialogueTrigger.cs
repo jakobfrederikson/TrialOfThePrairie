5bfca65672c0fe893846ebf07589f022b424091e
Jakob <jakobfred1999@gmail.com>
2023-05-30 12:07:09 +1200

    Dialogue system working in debug mode

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -6,8 +6,40 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
 
+    private BoxCollider boxCollider;
+    private bool inTrigger;
+    private bool isPlayer;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                TriggerDialogue();
+            if (Input.GetKeyDown(KeyCode.Q))
+                FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        }
+    }
+
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+        isPlayer = other.tag == "Player" ? true : false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+        isPlayer = other.tag == "Player" ? true : false;
     }
 }
