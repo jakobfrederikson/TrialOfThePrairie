dd783593477de51c81cac98d205109ff0b7ebbf8
Jakob <jakobfred1999@gmail.com>
2023-06-08 14:39:09 +1200

    Begin orb destroy text

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -1,4 +1,6 @@
 ﻿using StarterAssets;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -6,5 +8,18 @@ namespace Assets.Scripts
     public abstract class Orb : MonoBehaviour
     {
         [SerializeField] internal FirstPersonController firstPersonController;
+        [SerializeField] public TextMeshProUGUI orbDestroyText;
+        [SerializeField] public Animator animator;
+
+        private void Start()
+        {
+            orbDestroyText.enabled = false;
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("I've been destroyed!");
+            animator.SetBool("IsVisible", true);
+        }
     }
 }
